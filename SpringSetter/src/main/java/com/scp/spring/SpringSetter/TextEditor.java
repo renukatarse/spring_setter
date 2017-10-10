a7564f4bc4c7fcb562e5d4f5755a6301395f3202
renuka <renutarse1@gmail.com>
2017-10-10 14:08:54 +0530

    spring_setter commit

---
+++ SpringSetter/src/main/java/com/scp/spring/SpringSetter/TextEditor.java
@@ -0,0 +1,16 @@
+package com.scp.spring.SpringSetter;
+	public class TextEditor {
+		   private SpellChecker spellChecker;
+
+		   public void setSpellChecker(SpellChecker spellChecker) {
+		      System.out.println("Inside setSpellChecker." );
+		      this.spellChecker = spellChecker;
+		   }
+		   public SpellChecker getSpellChecker() {
+		      return spellChecker;
+		   }
+		   public void spellCheck() {
+		      spellChecker.checkSpelling();
+		   }
+		}
+
